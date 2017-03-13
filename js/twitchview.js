8b5cf5aa64f87fb7516029068e56a106015681ce
Miles Clark <mmclark@calpoly.edu>
2017-03-13 11:37:57 -0700

    Refactored test call into object

--- js/twitchview.js
+++ js/twitchview.js
@@ -1,13 +1,28 @@
-console.log("hello to twitch");
 
-var proxy = 'https://cors-anywhere.herokuapp.com/';
-var url = 'https://wind-bow.gomix.me/twitch-api/channels/freecodecamp/';
+var twitchy = {
+    proxy: 'https://cors-anywhere.herokuapp.com/',
+    url: 'https://wind-bow.gomix.me/twitch-api/channels/freecodecamp/',
 
-$.getJSON(proxy + url, function(data) {
-    console.log(data);
-}).done(function() {
-    console.log("done");
-}).fail(function() {
-    console.log("fail");
-    console.log(arguments);
-});
+    test: function() {
+        $.getJSON(this.proxy + this.url, function(data) {
+            console.log(data);
+        }).done(function() {
+            console.log("done");
+        }).fail(function() {
+            console.log("fail");
+            console.log(arguments);
+        });
+    },
+
+    getChannels: function () {
+        console.log("getChannels: not implemented yet");
+    },
+
+    getChannelInfo: function () {
+        console.log("getChannelInfo not implemented yet");
+    }
+};
+
+twitchy.test();
+
+
