cebe0e29ecf205b2dac4175974cf455f4db506fe
Miles Clark <mmclark@calpoly.edu>
2017-03-29 09:54:24 -0700

    Updates from other repo

--- js/twitchview.js
+++ js/twitchview.js
@@ -2,14 +2,81 @@ var TWITCHY = TWITCHY || {};
 
 
 TWITCHY.Main = (function() {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiurl = 'https://wind-bow.gomix.me/twitch-api';  // channels/freecodecamp/';
+  var cardtemplate =
+      '<div class="col-sm-6"> \
+          <div class="card"> \
+            <div class="card-block">\
+              <h3 class="card-title">{{STREAMTITLE}}</h3>\
+              <img  style="float:right; margin:1em;" src="{{STREAMIMAGE}}"  alt="Image for twitch stream"> \
+              <p class="card-text">{{STREAMTEXT}}</p>\
+              <a href="#" class="btn btn-primary">Go somewhere</a>\
+            </div>\
+          </div>\
+        </div>';
 
-    function hello() {
-        console.log('hello');
-    }
+  function hello() {
+    console.log('hello from twitchy');
+  }
 
-    return {
-        hello: hello
-    }
+  function listStreams() {
+    console.log('listStreams ENTER');
+    var url = proxy + apiurl + '/streams/featured';
+    var fccurl = proxy + apiurl + '/streams/freecodecamp';
+
+    $.getJSON(fccurl, function(data) {
+      console.log("fcc info---------------------------");
+      console.log(data);
+      if (data.stream === null) {
+	console.log("fcc is offline channel url:", data._links.channel);
+	$.getJSON(proxy+apiurl+'/channels/freecodecamp', function(data) {
+	  console.log("channel info ----------------------------");
+	  console.log(data);
+
+	  var markup = cardtemplate
+	    .replace("{{STREAMTITLE}}", data.display_name)
+	    .replace("{{STREAMTEXT}}", data.status)
+	    .replace("{{STREAMIMAGE}}", data.logo);
+	  $(markup).appendTo(streams);
+
+	}).fail(function() {
+	  console.log('getting channel info failed');
+	});
+      }
+
+      // var streams = $('#streams');
+      // for (var i=0; i < data.featured.length; i++) {
+      // 	var markup = cardtemplate
+      // 	    .replace("{{STREAMTITLE}}", data.featured[i].title)
+      // 	    .replace("{{STREAMTEXT}}", data.featured[i].text)
+      // 	    .replace("{{STREAMIMAGE}}", data.featured[i].image);
+      // 	$(markup).appendTo(streams);
+      // }
+    }).fail(function() {
+      console.log('call to', url, 'failed');
+    });
+
+    $.getJSON(url, function(data) {
+      console.log("featured info---------------------------");
+      console.log(data);
+      var streams = $('#streams');
+      for (var i=0; i < data.featured.length; i++) {
+	var markup = cardtemplate
+	    .replace("{{STREAMTITLE}}", data.featured[i].title)
+	    .replace("{{STREAMTEXT}}", data.featured[i].text)
+	    .replace("{{STREAMIMAGE}}", data.featured[i].image);
+	$(markup).appendTo(streams);
+      }
+    }).fail(function() {
+      console.log('call to', url, 'failed');
+    });
+  }
+
+  return {
+    hello: hello,
+    listStreams: listStreams
+  };
 
 })();
 
@@ -38,5 +105,4 @@ var oldtwitchy = {
 };
 
 TWITCHY.Main.hello();
-
-
+TWITCHY.Main.listStreams();
